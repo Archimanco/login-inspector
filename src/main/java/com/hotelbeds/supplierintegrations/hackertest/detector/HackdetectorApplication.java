5b2d90ebba1da8311096083a0bade4c5eac97396
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-05 11:13:46 +0200

    ADD project foundation

---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackdetectorApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackdetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackdetectorApplication.class, args);
+	}
+
+}
