05331ebe6cd128565e323dc0b1a3f87f0c687bd4
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-05 11:54:52 +0200

    ADD TimeCalculator as utils

---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/utils/TimeCalculator.java
@@ -0,0 +1,15 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector.utils;
+
+import java.util.Date;
+
+/**
+ * Write a function that returns the number of minutes (rounded down) between two
+ * timestamps  time1 and  time2 in RFC 2822 format (ie: Thu, 21 Dec 2000 16:01:07 +0200).
+ * Don’t forget about the time zones
+ */
+public class TimeCalculator {
+
+    public long differenceInMinutesBetween(Date start, Date end){
+
+    }
+}
---
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/utils/TimeCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeCalculatorTest {
+
+    @Test
+    void differenceInMinutesBetween() {
+    }
+}
