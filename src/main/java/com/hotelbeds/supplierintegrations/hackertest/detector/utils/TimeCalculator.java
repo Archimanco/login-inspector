6b5adda21e1030d9f9dad44e53b4386638b9ad86
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-05 13:43:46 +0200

    UPDATE TimeCalculator

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/utils/TimeCalculator.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/utils/TimeCalculator.java
@@ -1,16 +1,47 @@
 package com.hotelbeds.supplierintegrations.hackertest.detector.utils;
 
-import java.util.Date;
+import lombok.Value;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * Write a function that returns the number of minutes (rounded down) between two
  * timestamps  time1 and  time2 in RFC 2822 format (ie: Thu, 21 Dec 2000 16:01:07 +0200).
  * Don’t forget about the time zones
  */
+@Value
 public class TimeCalculator {
 
-    public long differenceInMinutesBetween(Date start, Date end){
-        // TODO
-        return 0;
+    TimeZone timeZone;
+
+    private static String FORMAT_RFC2822 = "EEE, dd MMM yyyy HH:mm:ss zzz";
+    private static int MIN_IN_SECONDS = 60;
+    private static int MILLIS_IN_SECONDS = 1000;
+    private static int MILLIS_IN_MINUTES = MILLIS_IN_SECONDS * MIN_IN_SECONDS;
+
+
+    public String differenceInMinutesBetween(Timestamp start, Timestamp end){
+
+        long differenceInMilliseconds = Math.abs(end.getTime() - start.getTime()); // milliseconds
+        long differenceInMinutes = differenceInMilliseconds / MILLIS_IN_MINUTES;
+        long differenceInSecondsRoundedDownToMinutes = differenceInMinutes * MIN_IN_SECONDS;
+
+        Instant instantFromDifference = Instant.ofEpochSecond(differenceInSecondsRoundedDownToMinutes);
+        ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instantFromDifference, timeZone.toZoneId());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FORMAT_RFC2822);
+        /* old formats
+            SimpleDateFormat dateFormat = new SimpleDateFormat(FORMAT_RFC2822);
+            dateFormat.setTimeZone(this.timeZone);
+            return dateFormat.format(new Date(differenceInSecondsRoundedDownToMinutes));
+        */
+
+        return zonedDateTime.format(formatter);
+
     }
+
+
 }
