8c8e772463cf344bb60f63d6bdbb49d07862f0e1
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-05 11:21:30 +0200

    ADD HackDetector Interface

---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
@@ -0,0 +1,9 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector;
+
+// change to DetectorOperations and make a REST API?  -   AlGut [05/07/2022]
+
+public interface HackerDetector {
+
+    String parseLine(String line);
+}
+
