dfeb36766d052f5e8d358c20aeec032fb8c63685
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-05 11:26:14 +0200

    ADD HackerDetector + Test

---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
@@ -0,0 +1,9 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector;
+
+public class HackerDetector implements HackerDetectable{
+
+    @Override
+    public String parseLine(String line) {
+        return null;
+    }
+}
---
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectorTest.java
@@ -0,0 +1,12 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HackerDetectorTest {
+
+    @Test
+    void parseLine() {
+    }
+}
