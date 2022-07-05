ad78aaac8020b51093bc9b68d5660eca1dbe1e4a
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-05 11:25:03 +0200

    UPDATE hacker detect interface

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectable.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectable.java
@@ -1,8 +1,10 @@
 package com.hotelbeds.supplierintegrations.hackertest.detector;
 
 // change to DetectorOperations and make a REST API?  -   AlGut [05/07/2022]
+// since we are going the self implementation route, rather than exposing/injecting via Spring
+// we will change the interface accordingly
 
-public interface HackerDetector {
+public interface HackerDetectable {
 
     String parseLine(String line);
 }
