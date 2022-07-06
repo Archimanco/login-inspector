78bed8033c199f6aa183dcfd1dbfb92782070c1d
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-06 19:19:53 +0200

    REFACTOR removed unnecessary code in hackerdetector

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
+import static com.hotelbeds.supplierintegrations.hackertest.detector.LogLine.isLogLineParseable;
+import static com.hotelbeds.supplierintegrations.hackertest.detector.LogLine.toLogLine;
+
 // food for thought: force it to be a "real" singleton to ensure that only one class keeps track of the logs?
 @Component
 public class HackerDetector implements HackerDetectable{
@@ -30,7 +33,6 @@ public class HackerDetector implements HackerDetectable{
         // since the method is called right a new log line is produced, we know that we are more or less on sync
         popLogsFiveMinutesBefore(logLine.getDate());
 
-
         int loginAttemptErrors =
                 storedLogs.entrySet().stream()
                         .filter(entry -> entry.getValue().getAction().compareTo(Action.SIGNIN_FAILURE) == 0)
@@ -46,30 +48,5 @@ public class HackerDetector implements HackerDetectable{
     private void popLogsFiveMinutesBefore(long date) {
         long newTimeWindowStart = date - MINUTES_TO_WATCH_IN_MILLIS;
         storedLogs.headMap(newTimeWindowStart).clear();
-    }
-
-    public static boolean isLogLineParseable(String lineToTest) {
-        // TODO
-        // check number of groups splitted by commas -> should be 4
-        // check if timestamp ( epoch ) is valid
-        // etc
-        return true;
-    }
-
-    public static LogLine toLogLine(String lineToParse) {
-        // ip,date,action,username
-        final int ipPosition = 0;
-        final int datePosition = 1;
-        final int actionPosition = 2;
-        final int usernamePosition = 3;
-
-        String[] splittedGroups = lineToParse.split("[,]");
-
-        return new LogLine(
-                splittedGroups[ipPosition],
-                Long.parseLong(splittedGroups[datePosition]),
-                Action.fromString(splittedGroups[actionPosition]),
-                splittedGroups[usernamePosition]
-        );
     }
 }
