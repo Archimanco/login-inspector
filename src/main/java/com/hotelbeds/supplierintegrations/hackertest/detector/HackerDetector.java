488137abca9d2ffe5c9ba54a862d1211f9ab5507
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-07 01:55:37 +0200

    UPDATE Hackerdetector logic & Tests

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
@@ -2,18 +2,16 @@ package com.hotelbeds.supplierintegrations.hackertest.detector;
 
 import org.springframework.stereotype.Component;
 
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
-
-import static com.hotelbeds.supplierintegrations.hackertest.detector.LogLine.isLogLineParseable;
-import static com.hotelbeds.supplierintegrations.hackertest.detector.LogLine.toLogLine;
 
 // food for thought: force it to be a "real" singleton to ensure that only one class keeps track of the logs?
 @Component
 public class HackerDetector implements HackerDetectable{
 
-    // will use a treemap to have our data sorted
-    TreeMap<Long,LogLine> storedLogs;
+        // will use a treemap to have our data sorted
+    TreeMap<Long,LogLine> storedLogs = new TreeMap<>();
 
     private static int MAX_LOGIN_ATTEMPTS_ALLOWED = 5;
     private static long MINUTES_TO_WATCH_IN_MILLIS = 5 * 60 *1000;
@@ -21,21 +19,21 @@ public class HackerDetector implements HackerDetectable{
     @Override
     public String parseLine(String lineInput) {
 
-        // TODO
-        if(!isLogLineParseable(lineInput)) {
-            // Do Something if line is corrupted or does not make sense?
-            // Ask if error should be thrown thus adjusting the interface
-        }
+        LogLine logLine = LogLine.toLogLine(lineInput);
 
-        LogLine logLine = toLogLine(lineInput);
+            // log line is corrupted or bad formatted
+        if(logLine == null)
+            return null;
 
-        // we need to hold up to 5 minutes of login, since this is just the timeframe to watch!!
-        // since the method is called right a new log line is produced, we know that we are more or less on sync
+            // we need to hold up to 5 minutes of login, since this is just the timeframe to watch!!
+            // since the method is called right a new log line is produced, we know that we are more or less on sync
         popLogsFiveMinutesBefore(logLine.getDate());
+        storedLogs.put(logLine.getDate(),logLine);
 
         int loginAttemptErrors =
                 storedLogs.entrySet().stream()
-                        .filter(entry -> entry.getValue().getAction().compareTo(Action.SIGNIN_FAILURE) == 0)
+                        .filter(entry -> entry.getValue().getIp().equals(logLine.getIp()))
+                        .filter(entry -> entry.getValue().getAction().getText().equals(Action.SIGNIN_FAILURE.getText()))
                         .collect(Collectors.toList())
                         .size();
 
@@ -47,6 +45,7 @@ public class HackerDetector implements HackerDetectable{
 
     private void popLogsFiveMinutesBefore(long date) {
         long newTimeWindowStart = date - MINUTES_TO_WATCH_IN_MILLIS;
+        SortedMap<Long, LogLine> testvariable = storedLogs.headMap(newTimeWindowStart);
         storedLogs.headMap(newTimeWindowStart).clear();
     }
 }
--- src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectorTest.java
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetectorTest.java
@@ -1,28 +1,137 @@
 package com.hotelbeds.supplierintegrations.hackertest.detector;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 class HackerDetectorTest {
 
-    @Test
-    void parseLine() {
+    private HackerDetector hackerDetector;
+
+    @BeforeEach
+    void setUp() {
+        hackerDetector = new HackerDetector();
     }
 
     @Test
-    void testParseLine() {
+    void testParseLine_erasesRecordsOlderThan5Minutes() {
+        List<String> logLinesAsString = new ArrayList<String>(
+                Arrays.asList(
+                        "183.238.9.180,133602947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133612947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133622947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133632947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133634047,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133634247,SIGNIN_FAILURE,Tony.Stark"
+                        ));
+        for (String logLineAsString : logLinesAsString) {
+            hackerDetector.parseLine(logLineAsString);
+        }
+
+        assertThat("Signing failures older than 5 minutes are ignored",
+                hackerDetector.parseLine("183.238.9.180,134002947,SIGNIN_FAILURE,Tony.Stark"),
+                is(nullValue()));
     }
 
     @Test
-    void toLogLine() {
+    void testParseLine_differentIpsAreEvaluatedSepparately() {
+        List<String> logLinesAsString = new ArrayList<String>(
+                Arrays.asList(
+                        "183.238.9.180,133602947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.181,133612947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.182,133633947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.183,133644947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.184,133655947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.185,133666947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.186,133677947,SIGNIN_FAILURE,Tony.Stark"
+                ));
+        for (String logLineAsString : logLinesAsString) {
+            hackerDetector.parseLine(logLineAsString);
+        }
+
+        assertThat("Ips are evalauted separately",
+                hackerDetector.parseLine("183.238.9.180,133687947,SIGNIN_FAILURE,Tony.Stark"),
+                is(nullValue()));
     }
 
     @Test
-    void isLogLineParseable() {
+    void testParseLine_supusiciousIpsAreCaught() {
+        List<String> logLinesAsString = new ArrayList<String>(
+                Arrays.asList(
+                        "183.238.9.180,133602947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133612947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133633947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133644947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133655947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133666947,SIGNIN_FAILURE,Tony.Stark",
+                        "183.238.9.180,133677947,SIGNIN_FAILURE,Tony.Stark"
+                ));
+        for (String logLineAsString : logLinesAsString) {
+            hackerDetector.parseLine(logLineAsString);
+        }
+
+        assertThat("Ip 183.238.9.180 is suspicious and is beign caught",
+                hackerDetector.parseLine("183.238.9.180,133687947,SIGNIN_FAILURE,Tony.Stark"),
+                is("183.238.9.180"));
     }
 
     @Test
-    void testToLogLine() {
+    void testParseLine_successLoginsAreNotEvaluatedAsSuspicious() {
+        List<String> logLinesAsString = new ArrayList<String>(
+                Arrays.asList(
+                        "183.238.9.180,133602947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133612947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133633947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133644947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133655947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133666947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133677947,SIGNIN_SUCCESS,Tony.Stark"
+                ));
+        for (String logLineAsString : logLinesAsString) {
+            hackerDetector.parseLine(logLineAsString);
+        }
+
+        assertThat("Ip 183.238.9.180 is not suspicious",
+                hackerDetector.parseLine("183.238.9.180,133687947,SIGNIN_SUCCESS,Tony.Stark"),
+                is(nullValue()));
     }
+
+    @Test
+    void testParseLine_evaluateKnownLogs() {
+        List<String> logLinesAsString = new ArrayList<String>(
+                Arrays.asList(
+                        "183.238.9.180,133602947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133612947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133613947,SIGNIN_FAILURE,Mr.Hacker",
+                        "183.238.9.180,133633947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133644947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133655947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133666947,SIGNIN_SUCCESS,Tony.Stark",
+                        "183.238.9.180,133677947,SIGNIN_SUCCESS,Tony.Stark"
+                ));
+        for (String logLineAsString : logLinesAsString) {
+            hackerDetector.parseLine(logLineAsString);
+        }
+
+        assertThat("Ip 183.238.9.180 is not suspicious",
+                hackerDetector.parseLine("183.238.9.180,133687947,SIGNIN_SUCCESS,Tony.Stark"),
+                is(nullValue()));
+
+        hackerDetector.parseLine("183.238.9.180,133687947,SIGNIN_FAILURE,Mr.Hacker");
+        hackerDetector.parseLine("183.238.9.180,133688947,SIGNIN_FAILURE,Mr.Hacker");
+        hackerDetector.parseLine("183.238.9.180,133689947,SIGNIN_FAILURE,Mr.Hacker");
+        hackerDetector.parseLine("183.238.9.180,133690947,SIGNIN_FAILURE,Mr.Hacker");
+
+        assertThat("Ip 183.238.9.180 is suspicious",
+                hackerDetector.parseLine("183.238.9.180,133691947,SIGNIN_FAILURE,Mr.Hacker"),
+                is("183.238.9.180"));
+    }
+
 }
