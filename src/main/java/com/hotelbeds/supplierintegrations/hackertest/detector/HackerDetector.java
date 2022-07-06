ca769498d5ac107673c4a3084f92b189976eab95
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-06 18:21:25 +0200

    UPDATE rework detector logic

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/HackerDetector.java
@@ -1,15 +1,19 @@
 package com.hotelbeds.supplierintegrations.hackertest.detector;
 
-import com.hotelbeds.supplierintegrations.hackertest.utils.TimeCalculator;
+import org.springframework.stereotype.Component;
 
-import java.time.ZonedDateTime;
-import java.util.List;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
+// food for thought: force it to be a "real" singleton to ensure that only one class keeps track of the logs?
+@Component
 public class HackerDetector implements HackerDetectable{
 
+    // will use a treemap to have our data sorted
+    TreeMap<Long,LogLine> storedLogs;
+
     private static int MAX_LOGIN_ATTEMPTS_ALLOWED = 5;
-    private static long MINUTES_TO_WATCH = 5l;
+    private static long MINUTES_TO_WATCH_IN_MILLIS = 5 * 60 *1000;
 
     @Override
     public String parseLine(String lineInput) {
@@ -22,14 +26,14 @@ public class HackerDetector implements HackerDetectable{
 
         LogLine logLine = toLogLine(lineInput);
 
-        List<LogLine> logLinesToTest =
-                getLastLoginAttempsByIpAfterDate(
-                        logLine.getIp(),
-                        logLine.getDate().minusMinutes(MINUTES_TO_WATCH));
+        // we need to hold up to 5 minutes of login, since this is just the timeframe to watch!!
+        // since the method is called right a new log line is produced, we know that we are more or less on sync
+        popLogsFiveMinutesBefore(logLine.getDate());
+
 
         int loginAttemptErrors =
-                logLinesToTest.stream()
-                        .filter(line -> line.getAction().compareTo(Action.SIGNIN_FAILURE) == 0)
+                storedLogs.entrySet().stream()
+                        .filter(entry -> entry.getValue().getAction().compareTo(Action.SIGNIN_FAILURE) == 0)
                         .collect(Collectors.toList())
                         .size();
 
@@ -39,9 +43,9 @@ public class HackerDetector implements HackerDetectable{
             return null;
     }
 
-    private List<LogLine> getLastLoginAttempsByIpAfterDate(String ip, ZonedDateTime start) {
-        // TODO
-        return null;
+    private void popLogsFiveMinutesBefore(long date) {
+        long newTimeWindowStart = date - MINUTES_TO_WATCH_IN_MILLIS;
+        storedLogs.headMap(newTimeWindowStart).clear();
     }
 
     public static boolean isLogLineParseable(String lineToTest) {
@@ -63,7 +67,7 @@ public class HackerDetector implements HackerDetectable{
 
         return new LogLine(
                 splittedGroups[ipPosition],
-                TimeCalculator.stringInRFC2822ToZonedDateTime(splittedGroups[datePosition]),
+                Long.parseLong(splittedGroups[datePosition]),
                 Action.fromString(splittedGroups[actionPosition]),
                 splittedGroups[usernamePosition]
         );
