14d3e980d977ec5b3761730f0234c054e5ec8e93
Alejandro Gutiérrez <alejandro.gutierrez.delarica@hotmail.es>
2022-07-07 01:55:14 +0200

    REFACTOR got rid of unnecessary comments

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/utils/TimeCalculator.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/utils/TimeCalculator.java
@@ -4,12 +4,6 @@ import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.ZonedDateTime;
 
-/**
- * Write a function that returns the number of minutes (rounded down) between two
- * timestamps  time1 and  time2 in RFC 2822 format (ie: Thu, 21 Dec 2000 16:01:07 +0200).
- * Don’t forget about the time zones
- */
-
 public class TimeCalculator {
 
     private static int MIN_IN_SECONDS = 60;
